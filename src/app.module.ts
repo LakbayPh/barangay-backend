bf6bec52b803970294802fe07aeb505e29482af2
Eastly1997 <jw.bodoso@gmail.com>
2026-05-03 19:37:24 +0800

    feat(auth): implement JWT authentication with guards, swagger integration, and env config"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRES_IN: Joi.string().required(),
+      }),
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController], // 👈 THIS WAS MISSING
 })
 export class AppModule {}
